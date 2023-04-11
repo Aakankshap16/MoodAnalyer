6b46d079387db9582dd4afb569093b079c3f6fad
Aakankshap16 <aakankshap971@gmail.com>
2023-04-11 15:32:27 +0530

    ability to analyse mood

---
+++ AnalyserProgram/AbilityToAnalyse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyserProgram
+{
+   public class AbilityToAnalyse
+    {
+        public string AnalyseMood(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "Sad Mood";
+            }
+            else
+            {
+                return "Happy Mood";
+            }
+
+        }
+    }
+}
