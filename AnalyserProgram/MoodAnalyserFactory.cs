b1a87ca8135b7daf3c2addd4ecd84941c9532bc8
Aakankshap16 <aakankshap971@gmail.com>
2023-04-12 14:32:45 +0530

    Use Reflection and throw exception

---
+++ AnalyserProgram/MoodAnalyserFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace AnalyserProgram
+{
+    public class MoodAnalyserFactory
+    {
+        public static object CreateMoodAnalyse(string className, string constructorName)
+        {
+           
+            string pattern = @"." + constructorName + @"\(\)$";
+            Match result = Regex.Match(pattern, className);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly execute = Assembly.GetExecutingAssembly();
+                    Type checktype = execute.GetType(className);
+                    return Activator.CreateInstance(checktype);
+                }
+                catch
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.No_Such_Class, "Class not found");
+
+                }
+            }
+            else
+            {
+              
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.No_Such_Method, "Constructor is not found");
+
+            }
+        }
+    }
+}
